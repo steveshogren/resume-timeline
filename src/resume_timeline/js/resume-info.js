dfe4de11f82746296330c0f71e5b7794dfdcb4bc
Steve <madklowns@gmail.com>
2012-12-24 12:29:05 -0500

    changing more on the skills

--- src/resume_timeline/js/resume-info.js
+++ src/resume_timeline/js/resume-info.js
@@ -5,6 +5,7 @@ $(function (){
         {
             title: "Software Developer",
             company: "AWE",
+            skills: ["c#", "vb.net", "asp.net", "tdd", "nunit", "sql server", "web services", "mysql", "actionscript", "php"],
             location: "March 2011 - July 2012",
             summary: ["Assumed complete responsibility for, tested, and completed an unfinished legacy system for bi-directional data synchronization and statistics collection between independent kiosks with inconsistent Internet connectivity and unreliable date/time information in VB.NET and C#. Learned to maintain and repair several legacy versioned SOAP web services that had to be carefully dealt with due to a assortment of out-of-date kiosks in the field. Setup an internal wiki and helped maintain its Linux server. Motivated the team to setup and later helped to configure the CC.NET site for our CI server. ",
                       "Developed an authorization system, allowing customers to easily restrict certain areas of kiosk management using basic default profiles or to generate a completely customized solution. Assisted with migrating kiosk code base from using Access to Mysql, and from Windows XP to Windows 7. Leveraged python to script out small maintenance tasks. ",
@@ -13,15 +14,22 @@ $(function (){
         {
             title: "Software Developer",
             company: "ReminderMedia",
+            skills: ["php", "phpunit", "tdd", "apache", "pair-programming", "mysql", "linux"],
             location: "October 2009 - March 2011",
             summary: ["Developed clean code using test-driven design while striving for pragmatically high code coverage in PHP using phpunit. Trained three new developers through pair programming and self-designed evaluation documentation. Revised interview process, authoring a partially completed project for candidates to complete before the interview, giving them time to do their best and show more then just the typical “fizzbuzz”. Implemented “two pairs of eyes per line” code review practices with all team members using Kiln.",
                       "Refactored an authorization and authentication module for our external site using modern security practices and methods. Remodeled legacy systems by: bringing them under test, refactoring for clarity, and breaking apart where needed to better meet the underlying core domain model. Pair programmed a team management module for our customer service department that tripled conversion speed. Partnered with another developer in testing and rewriting the core business printing module, which is still responsible for composing and printing hundreds of thousands of uniquely customized magazines every week. ",
                       "Made architecture changes allowing our tests to be written with active record fakes by modifying our real active record entities to use injected state, reducing total test run time significantly. Sought out faster ways to run integration tests by converting the database to utilize a transactional setup, reducing test load times by a second each."],
         },
     ],
+    makeSkillsHtml = function(skills) {
+        return '<span class="btn-other">' + skills.join('</span> <span class="btn-other">') + "</span>";
+    },
     makeJobToHtml = function(job) {
         return "<h5>" + job.title + " <span class=\"muted\">" + job.company + " - " + job.location + "</span></h5><p>" + job.summary.join("</p><p>") + "</p>";
+    },
+    strReduce = function(list, func, join) {
+        $.map(list, func).join(join);
     };
-    $("#xp").html($.map(job_info, makeJobToHtml).join(' ')); 
-    $("#skills").html('<a class="active">' + skills.join('</a> <a class="active">')); 
+    $("#xp").html(strReduce(job_info, makeJobToHtml, ' ')); 
+    $("#skills").html(strReduce(skills, makeSkillsHtml, ' ')); 
 });
