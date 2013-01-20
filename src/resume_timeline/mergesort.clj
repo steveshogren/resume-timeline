e829ccf94f7709f294ac323b3369b4144b322285
Steve <madklowns@gmail.com>
2013-01-20 17:00:04 -0500

    Fixing the mergesort to handle duplicates

--- src/resume_timeline/mergesort.clj
+++ src/resume_timeline/mergesort.clj
@@ -7,12 +7,16 @@
 ;;  Mergesort(A [1, n])
 ;;    Merge (Mergesort(A[1,[n/2]]), Mergesort(A[[n/2]+1,n]))
 
-(defn merge-sort [list]
+(defn merge-sort
+  [list]
   (cond
    (== (count list) 1) list
-   :else (merge-lists (map merge-sort (split-at (floor (/ (count list) 2)) list)))))
+   :else (merge-lists
+          (map merge-sort
+               (split-at (floor (/ (count list) 2)) list)))))
 
-(defn merge-lists [[orig-left-list orig-right-list]]
+(defn merge-lists
+  [[orig-left-list orig-right-list]]
   (loop [lefts orig-left-list
          rights orig-right-list
          return []]
@@ -21,11 +25,10 @@
      (empty? rights) (concat return lefts)
      :else (let [left (first lefts)
                  right (first rights)]
-             (cond
-              (< left right)  (recur (rest lefts)
-                                     rights
-                                     (conj return left))
-              (> left right)  (recur lefts
-                                     (rest rights)
-                                     (conj return right)))))))
+             (cond (< left right)  (recur (rest lefts)
+                                          rights
+                                          (conj return left))
+                   :else  (recur lefts
+                                 (rest rights)
+                                 (conj return right)))))))
 
--- test/resume_timeline/test/mergesort.clj
+++ test/resume_timeline/test/mergesort.clj
@@ -5,6 +5,7 @@
 
 (deftest test-sort
   (is (= [1 2 3 4] (merge-sort [4 2 1 3])))
+  (is (= [1 3 3 4 4] (merge-sort [4 4 1 3 3])))
   (is (= [1] (merge-sort [1])))
   (is (= [1 2 3 4 5 6 7 8 9] (merge-sort [9 7 5 3 1 2 8 6 4])))
   (is (= [1 2 3 4] (merge-sort [1 2 3 4]))))
